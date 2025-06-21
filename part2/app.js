5f1f8386d22b0d9bc0c218d58a9645310ef8c558
SSuhani <a1923326@adelaide.edu.au>
2025-06-21 05:16:07 +0000

    [automatic commit] 2025-06-21 05:16:07 UTC 122.108.118.51

--- part2/app.js
+++ part2/app.js
@@ -15,5 +15,9 @@ const userRoutes = require('./routes/userRoutes');
 app.use('/api/walks', walkRoutes);
 app.use('/api/users', userRoutes);
 
+
+
+
+
 // Export the app instead of listening here
 module.exports = app;
