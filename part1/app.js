3d0c27a5283fabf0d275e0c0756da6c5641f2518
SSuhani <a1923326@adelaide.edu.au>
2025-06-21 05:39:59 +0000

    [automatic commit] 2025-06-21 05:39:59 UTC 122.108.118.51

--- part1/app.js
+++ part1/app.js
@@ -1,46 +1,20 @@
-// Filename: app.js
-const express = require('express');
-const path = require('path');
-const logger = 'morgan';
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 
-// --- Database Connection ---
-const mysql = require('mysql2/promise');
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
 
-// Create a connection pool. This is more efficient than creating a new connection for every query.
-// !!! IMPORTANT: Replace with your actual MySQL database details from your dogwalks.sql !!!
-const db = mysql.createPool({
-    host: 'localhost',
-    user: 'your_mysql_user',       // e.g., 'root'
-    password: 'your_mysql_password', // e.g., 'password'
-    database: 'DogWalkService'       // The database name from your .sql file
-});
+var app = express();
 
-// Make the 'db' object available to all routes
-// By attaching it to the request object, our route files can access it.
-const app = express();
-app.use((req, res, next) => {
-    req.db = db;
-    next();
-});
-
-// Import your new API router
-const apiRouter = require('./routes/api');
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
-
+app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Use the routers
-app.use('/api', apiRouter); // All API routes will be handled by api.js
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-// Note: The original /api/dogs handler that was here has been moved to routes/api.js
-
 module.exports = app;
-
-// The server is typically started in a separate file (e.g., bin/www)
-// in an Express-generated project. If you are running this file directly with `node app.js`,
-// you would add the app.listen() call here.
